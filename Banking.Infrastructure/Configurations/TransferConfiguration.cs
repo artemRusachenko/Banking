e969396f035a8fbf784d5b54911331a98e2c3944
artemRusachenko <artemrusachenko@gmail.com>
2024-10-28 11:19:53 +0200

    feat: add configurations for entities

---
+++ Banking.Infrastructure/Configurations/TransferConfiguration.cs
@@ -0,0 +1,31 @@
+﻿using Banking.Domain;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using Microsoft.EntityFrameworkCore;
+
+namespace Banking.Infrastructure.Configurations
+{
+    public class TransferConfiguration : IEntityTypeConfiguration<Transfer>
+    {
+        public void Configure(EntityTypeBuilder<Transfer> builder)
+        {
+            ArgumentNullException.ThrowIfNull(builder);
+
+            builder.HasKey(t => t.Id);
+
+            builder.Property(t => t.Amount)
+                .HasColumnType("decimal(18,2)")
+                .IsRequired();
+
+            builder.Property(t => t.TransferDate)
+                .IsRequired();
+
+            builder.HasOne(t => t.FromAccount)
+                .WithMany(a => a.TransferFrom)
+                .HasForeignKey(t => t.FromAccountId);
+
+            builder.HasOne(t => t.ToAccount)
+                .WithMany(a => a.TransferTo)
+                .HasForeignKey(t => t.ToAccountId);
+        }
+    }
+}
