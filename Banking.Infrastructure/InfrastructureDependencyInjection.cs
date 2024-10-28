0f6ee95a4a0a7852013ecc13bed35fc28dda9ab9
artemRusachenko <artemrusachenko@gmail.com>
2024-10-28 11:21:27 +0200

    feat: add db context

---
+++ Banking.Infrastructure/InfrastructureDependencyInjection.cs
@@ -0,0 +1,18 @@
+﻿using Banking.Infrastructure.Data;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Banking.Infrastructure
+{
+    public static class InfrastructureDependencyInjection
+    {
+        public static IServiceCollection AddInfrastructureServices(this IServiceCollection services, IConfiguration config)
+        {
+            services.AddDbContext<ApplicationDbContext>(options =>
+                options.UseSqlServer(config.GetConnectionString("BankingDatabase")));
+
+            return services;
+        }
+    }
+}
