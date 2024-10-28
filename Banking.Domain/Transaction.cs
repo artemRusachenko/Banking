d03d0d7fbfae162e32f26746d13373eb9094925b
artemRusachenko <artemrusachenko@gmail.com>
2024-10-28 11:19:05 +0200

    feat: add entities to the Domain layer

---
+++ Banking.Domain/Transaction.cs
@@ -0,0 +1,35 @@
+﻿namespace Banking.Domain
+{
+    public enum TransactionType
+    {
+        Deposit,
+        Withdrawal,
+        Transfer
+    }
+
+    public class Transaction
+    {
+        public Guid Id { get; set; }
+        public Guid AccountId { get; set; }
+        public TransactionType TransactionType { get; set; }
+        public decimal Amount { get; set; }
+        public DateTime TransactionDate { get; set; }
+        public string Description { get; set; }
+
+        public virtual Account? Account { get; set; }
+
+        public Transaction(Guid accountId, TransactionType transactionType, decimal amount, string? description)
+        {
+            Id = Guid.NewGuid();
+            AccountId = accountId;
+            TransactionType = transactionType;
+            Amount = amount;
+            TransactionDate = DateTime.UtcNow;
+
+            if (description == null)
+                description = $"{TransactionType} of {Amount}$ on {TransactionDate:yyyy-MM-dd HH:mm:ss} UTC";
+
+            Description = description;
+        }
+    }
+}
---
+++ Banking.Domain/Transfer.cs
@@ -0,0 +1,23 @@
+﻿namespace Banking.Domain
+{
+    public class Transfer
+    {
+        public Guid Id { get; set; }
+        public Guid FromAccountId { get; set; }
+        public Guid ToAccountId { get; set; }
+        public decimal Amount { get; set; }
+        public DateTime TransferDate { get; set; }
+
+        public virtual Account? FromAccount { get; set; }
+        public virtual Account? ToAccount { get; set; }
+
+        public Transfer(Guid fromAccountId, Guid toAccountId, decimal amount, DateTime transferDate)
+        {
+            Id = Guid.NewGuid();
+            FromAccountId = fromAccountId;
+            ToAccountId = toAccountId;
+            Amount = amount;
+            TransferDate = transferDate;
+        }
+    }
+}
