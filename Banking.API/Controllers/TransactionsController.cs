bbc17a882bed2809aa397cd1e87da4206976676a
artemRusachenko <artemrusachenko@gmail.com>
2024-10-29 21:03:12 +0200

    feat: add controllers

---
+++ Banking.API/Controllers/TransactionsController.cs
@@ -0,0 +1,34 @@
+﻿using Banking.Application.Transactions.Commands.Deposit;
+using Banking.Application.Transactions.Commands.Withdraw;
+using MediatR;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Banking.API.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class TransactionsController(IMediator mediator) : ControllerBase
+    {
+        [HttpPost("deposit")]
+        public async Task<IActionResult> Deposit([FromBody] DepositCommand command, CancellationToken cancellationToken)
+        {
+            var result = await mediator.Send(command, cancellationToken).ConfigureAwait(false);
+
+            if(result.IsSuccess)
+                return Ok(result.Value);
+
+            return BadRequest(new { error = result.Error?.Message ?? "An error occurred during depositing." });
+        }
+
+        [HttpPost("withdraw")]
+        public async Task<IActionResult> Withdraw([FromBody] WithdrawCommand command, CancellationToken cancellationToken)
+        {
+            var result = await mediator.Send(command, cancellationToken).ConfigureAwait(false);
+
+            if (result.IsSuccess)
+                return Ok(result.Value);
+
+            return BadRequest(new { error = result.Error?.Message ?? "An error occurred during withdrawing." });
+        }
+    }
+}
