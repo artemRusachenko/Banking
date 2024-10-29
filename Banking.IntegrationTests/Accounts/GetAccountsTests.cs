1122d3610aaa3b544cd06e1fcb8740ba55b5c1dc
artemRusachenko <artemrusachenko@gmail.com>
2024-10-29 11:05:45 +0200

    feat: add integration and unit tests for Account Management

---
+++ Banking.IntegrationTests/Accounts/GetAccountsTests.cs
@@ -0,0 +1,28 @@
+﻿#pragma warning disable CA1707
+using Banking.API.Accounts.Queries.GetAll;
+using Banking.Application.Accounts.Queries.GetAll;
+using Banking.Domain.Accounts;
+
+namespace Banking.IntegrationTests.Accounts
+{
+    public class GetAccountsTests : IntegrationTestBase
+    {
+        [Fact]
+        public async Task Should_Return_Accounts_From_Database()
+        {
+            //Arrange
+            await _context.Accounts.AddAsync(new Account("Test1")).ConfigureAwait(false);
+            await _context.Accounts.AddAsync(new Account("Test2")).ConfigureAwait(false);
+            await _unitOfWork.SaveChangesAsync().ConfigureAwait(false);
+
+            var handler = new GetAllQueryHandler(_accountRepository);
+
+            //Act
+            var result = await handler.Handle(new GetAllQuery(), CancellationToken.None).ConfigureAwait(false);
+
+            //Assert
+            Assert.True(result.IsSuccess);
+            Assert.Equal(2, result.Value!.Accounts.Count());
+        }
+    }
+}
---
+++ Banking.IntegrationTests/IntegrationTestBase.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using Banking.Domain.Accounts;
+using Banking.Domain.Data;
+using Banking.Infrastructure.Data;
+using Banking.Infrastructure.Repositories;
+using Microsoft.EntityFrameworkCore;
+
+namespace Banking.IntegrationTests
+{
+    public class IntegrationTestBase : IDisposable
+    {
+        internal readonly IApplicationDbContext _context;
+        internal readonly IAccountRepository _accountRepository;
+        internal readonly IUnitOfWork _unitOfWork;
+        private bool _disposed;
+
+        public IntegrationTestBase()
+        {
+            var options = new DbContextOptionsBuilder<ApplicationDbContext>()
+                .UseInMemoryDatabase(databaseName: Guid.NewGuid().ToString()).Options;
+
+            var applicationDbContext = new ApplicationDbContext(options);
+            _context = applicationDbContext;
+            _accountRepository = new AccountRepository(applicationDbContext);
+            _unitOfWork = new UnitOfWork(applicationDbContext);
+        }
+
+        public void Dispose()
+        {
+            Dispose(true);
+            GC.SuppressFinalize(this);
+        }
+
+        protected virtual void Dispose(bool disposing)
+        {
+            if (!_disposed)
+            {
+                if (disposing)
+                {
+                    ((ApplicationDbContext)_context)?.Database.EnsureDeleted();
+                    ((ApplicationDbContext)_context!)?.Dispose();
+                }
+
+                _disposed = true;
+            }
+        }
+
+        ~IntegrationTestBase()
+        {
+            Dispose(false);
+        }
+    }
+}
