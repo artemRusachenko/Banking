4cf1b3b5779c17a4b5db550d1dd983d07685bef0
artemRusachenko <artemrusachenko@gmail.com>
2024-10-29 11:03:26 +0200

    feat: add get all accounts handler

---
+++ Banking.Application/Accounts/Queries/GetAll/GetAllQuery.cs
@@ -0,0 +1,8 @@
+﻿using Banking.Application.Accounts.Queries.GetAll;
+using Banking.Application.Core;
+using MediatR;
+
+namespace Banking.Application.Accounts.Queries.GetAll
+{
+    public record GetAllQuery() : IRequest<Result<GetAllResult>>;
+}
